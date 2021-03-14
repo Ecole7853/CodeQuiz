9e547329eed4ac83793299aa7ad7f9b31c637c25
Eric Cole <ecole052785@gmail.com>
2021-03-14 16:22:18 -0400

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,26 @@
-// var startGame = 
-// var endGame = 
 var questions = [
-  {question1: "A very useful tool used during development and debugging for print content to the debugger is:"},
-  {question2: "The condition in an if / else statment is enclosed within ______."},
-  {question3: "Arrays in JavaScript can be used to store ______."},
-  {question4: "Commonly used data types DO NOT include:"}
+  {question: "A very useful tool used during development and debugging for print content to the debugger is:", 
+   answers: {a: "JavaScript", b: "terminal/bash",  c: "for loops",  d: "console.log"},
+   correctAnswer:"d"},
+  
+  {question: "The condition in an if / else statment is enclosed within ______.", 
+   answers: {a: "quotes", b: "curly brackets", c: "paranthesis", d: "square brackets"},
+   correctAnswer:"b"},
+  
+   {question: "Arrays in JavaScript can be used to store ______.",
+   answers: {a: "numbers and strings", b: "other arrays", c: "booleans", d: "all of the above"},
+   correctAnswer:"d"},
+  
+   {question: "Commonly used data types DO NOT include:",
+    answers: {a: "strings", b: "bookleans", c: "alerts", d: "numbers"},
+    correctAnswer:"c"}
 ]
-var answers = [
-  {answer1: {a: "JavaScript", b: "terminal/bash",  c: "for loops",  d: "console.log"}},
-  {answer2: {a: "quotes", b: "curly brackets", c: "paranthesis", d: "square brackets"}},
-  {answer3: {a: "numbers and strings", b: "other arrays", c: "booleans", d: "all of the above"}},
-  {answer4: {a: "strings", b: "bookleans", c: "alerts", d: "numbers"}}
-]
+var correctAnswersGiven = 0;
+var activeQuestion = 0;
 var highScore = document.querySelector("#highScore")
-var wrongAnswer = (secondsLeft = secondsLeft-10)
 var li = $("<li></li>")
 var container = $(".container");
-var secondsLeft = (60);
+var secondsLeft = 30;
 var timeEl = $("#time");
 var startButton =  document.querySelector("#start");
 function setTime() {
@@ -25,70 +29,95 @@ function setTime() {
       $('#time').html("Time:" + secondsLeft);
       if(secondsLeft <= 0) {
         clearInterval(timerInterval);
-        //sendMessage();
+        endGame();
       }
     }, 1000);
   }
-function question2() {
-  $(".startPage").css("display","none")
-  $("#questions").css("display","block")
-  $("#questionText").append(questions[1].question2);
-  $("#answer1").append(answers[1].answer2.a);
-  $("#answer2").append(answers[1].answer2.b);
-  $("#answer3").append(answers[1].answer2.c);
-  $("#answer4").append(answers[1].answer2.d);
+function endGame(){
+  var finalScore = correctAnswersGiven*secondsLeft
+  var submitBtn = document.querySelector("#submitBtn")
+  $("#finalScore").text("Your final score is " + finalScore.toString())
+  container.children().css("display","none")
+  $(".enterScore").css("display","block")
+  submitBtn.addEventListener("click", function(){
+    localStorage.setItem($("#name").val(), finalScore);
+    console.log($("#name"))
+  })
 }
-function question3() {
-  $(".startPage").css("display","none")
-  $("#questions").css("display","block")
-  $("#questionText").append(questions[2].question3);
-  $("#answer1").append(answers[2].answer3.a);
-  $("#answer2").append(answers[2].answer3.b);
-  $("#answer3").append(answers[2].answer3.c);
-  $("#answer4").append(answers[2].answer3.d);
-}
-function question4() {
-  $(".startPage").css("display","none")
-  $("#questions").css("display","block")
-  $("#questionText").append(questions[3].question3);
-  $("#answer1").append(answers[3].answer3.a);
-  $("#answer2").append(answers[3].answer3.b);
-  $("#answer3").append(answers[3].answer3.c);
-  $("#answer4").append(answers[3].answer3.d);
+function changeQuestion (){
+  activeQuestion++;
+  if (activeQuestion==questions.length){
+    endGame();
+  }
+  else {
+    $("#questionText").text(questions[activeQuestion].question);
+    $("#answer1").text(questions[activeQuestion].answers.a);
+    $("#answer2").text(questions[activeQuestion].answers.b);
+    $("#answer3").text(questions[activeQuestion].answers.c);
+    $("#answer4").text(questions[activeQuestion].answers.d);
+  }
 }
 startButton.addEventListener("click", function(){
+    setTime()
     $(".startPage").css("display","none")
     $("questionDetail").css("display", "none")
     $("#questions").css("display","block")
-    $("#questionText").append(questions[0].question1);
-    $("#answer1").append(answers[0].answer1.a);
-    $("#answer2").append(answers[0].answer1.b);
-    $("#answer3").append(answers[0].answer1.c);
-    $("#answer4").append(answers[0].answer1.d);
+    $("#questionText").text(questions[activeQuestion].question);
+    $("#answer1").text(questions[activeQuestion].answers.a);
+    $("#answer2").text(questions[activeQuestion].answers.b);
+    $("#answer3").text(questions[activeQuestion].answers.c);
+    $("#answer4").text(questions[activeQuestion].answers.d);
     li.addClass("#questions");
     document.querySelector("#answer1").addEventListener("click", function(){
-      $("#questionDetail").text("Wrong");
-      question2();
+      if (questions[activeQuestion].correctAnswer=="a"){
+        $("#questionDetail").text("Correct!");
+        correctAnswersGiven +=1;
+      }
+      else {
+        $("#questionDetail").text("Wrong!")
+        secondsLeft -=10;
+      }
+      changeQuestion();
     })
-    // document.querySelector("#answer2").addEventListener("click", function(){
-    //   wrongAnswer
-    //   question2();
-    // })
-    // document.querySelector("#answer3").addEventListener("click", function(){
-    //   wrongAnswer
-    //   question2();
-    // })
-    // document.querySelector("#answer4").addEventListener("click", function(){
-    //   wrongAnswer
-    //   question2();
-    // })
+    document.querySelector("#answer2").addEventListener("click", function(){
+      if (questions[activeQuestion].correctAnswer=="b"){
+        $("#questionDetail").text("Correct!");
+        correctAnswersGiven +=1;
+      }
+      else {
+        $("#questionDetail").text("Wrong!")
+        secondsLeft -=10;
+      }
+      changeQuestion();
+    })
+    document.querySelector("#answer3").addEventListener("click", function(){
+      if (questions[activeQuestion].correctAnswer=="c"){
+        $("#questionDetail").text("Correct!");
+        correctAnswersGiven +=1;
+      }
+      else {
+        $("#questionDetail").text("Wrong!")
+        secondsLeft -=10;
+      }
+      changeQuestion();
+    })
+    document.querySelector("#answer4").addEventListener("click", function(){
+      if (questions[activeQuestion].correctAnswer=="d"){
+        $("#questionDetail").text("Correct!");
+        correctAnswersGiven +=1;
+      }
+      else {$("#questionDetail").text("Wrong!")
+      secondsLeft -=10;
+      }
+      changeQuestion();
+    })
 }
 )
-
 highScore.addEventListener("click", function(e){
   e.preventDefault()
   container.children().css("display","none")
   $(".initials").css("display","block")
+  
 })
 // secondsLeft = secondsLeft-2 use this for incorrect answer (IMPORTANT)*******
 
@@ -100,3 +129,6 @@ highScore.addEventListener("click", function(e){
 // localStorage.setItem("count", count);
 // function to display storage of wins/losses
 // init function to replace score
+//   $("#questionDetail").text("Correct!");
+//<input type=text></input>
+//      secondsLeft -=10;
